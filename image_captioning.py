5ea340e4dbe41afaf547813c340557936ea102ea
soumyasucharitagh <150057513+soumyasucharitagh@users.noreply.github.com>
2026-03-14 01:05:37 +0530

    Update image_captioning.py

--- image_captioning.py
+++ image_captioning.py
@@ -5,14 +5,21 @@ from PIL import Image
 processor = BlipProcessor.from_pretrained(
     "Salesforce/blip-image-captioning-base"
 )
+
 model = BlipForConditionalGeneration.from_pretrained(
-    "Salesforce/blip-image-captioning-base"
+    "Salesforce/blip-image-captioning-base",
+    torch_dtype=torch.float16
 )
+
 model.eval()
 
 def generate_caption(image):
+
     inputs = processor(image, return_tensors="pt")
+
     with torch.no_grad():
         output = model.generate(**inputs)
+
     caption = processor.decode(output[0], skip_special_tokens=True)
+
     return caption
